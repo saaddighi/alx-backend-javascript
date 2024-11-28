e51fbcd9bfe80498cbb8c25e89d189e014c092d7
saaddighi <saaddighi20@gmail.com>
2024-11-28 19:18:10 +0000

    ok

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,9 +5,14 @@ const rl = redline.createInterface({
     output: process.stdout
 });
 
+process.on("SIGINT", () => {
+    console.log("\nThank you for using the program! Closing now...");
+    rl.close();
+    process.exit();
+})
+
 rl.question("Welcome to Holberton School, what is your name?"+"\n", 
     function(answer){
         console.log("Your name is:", answer);
-        console.log("This important software is now closing");
         rl.close()
 });
