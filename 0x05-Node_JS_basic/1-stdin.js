1a6a291a629bdfacdc759747149586cd3d481972
saaddighi <saaddighi20@gmail.com>
2024-11-29 12:07:36 +0000

    ok

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,26 @@
-const redline = require('readline')
+const readline = require('readline')
 
-const rl = redline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question("Welcome to Holberton School, what is your name?"+"\n", 
-    function(answer){
-        console.log("Your name is:", answer);
-        rl.close()
-});
-
-rl.on('SIGINT', function(){
-    console.log("\nThis important software is now closing");
-    process.exit(0)
-})
+if (process.stdin.isTTY){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    rl.question("Welcome to Holberton School, what is your name?\n", function(answer){
+            console.log("Your name is:", answer);
+            rl.close()
+    });
+    
+    rl.on('SIGINT', function(){
+        console.log("\nThis important software is now closing");
+        process.exit(0)
+    });
+} else{
+    let input = ""
+    process.stdin.on('data', function (data){
+        input = data.toString();
+    });
+    process.stdin.on('end', function(){
+        console.log("Your name is:", input.trim())
+    });
+}
